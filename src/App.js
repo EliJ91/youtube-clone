f6f8e0be95aea15df97906395306f835d22c103b
EliJ91 <59631669+EliJ91@users.noreply.github.com>
2021-02-10 16:28:39 -0500

    Update UI
    -Fixed landing page on widescreen monitors
    -Fixed z-index/positioning issues with material-UI icons

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import NewLoginUi from './components/Login/newLoginUi'
 import {BrowserRouter as Router,  Route} from "react-router-dom"
 
 
-
 function AppContainer(props) {
   return (
     <div className="app__page">
